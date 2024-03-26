d572e1ddf6b531eec70cce7b6bdd46f3cccdfdad
Marta Locmele <martamarijalocmele@gmail.com>
2024-03-26 18:44:47 +0200

    fixed modal

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,51 +1,60 @@
 "use client"
 import Link from 'next/link'
+import { useState } from 'react'
 import styles from './Header.module.css'
 import { useScroll } from '@/context/Scroll'
-import { useRouter, usePathname} from 'next/navigation'
+import { useRouter, usePathname } from 'next/navigation'
+import ModalMenu from '../ModalMenu/ModalMenu'
 
- 
+
 export default function Header() {
+
+  const [isMenuOpen, setMenuOpen] = useState(false);
 
   const pathname = usePathname()
 
   const isHomePage = pathname === '/';
-   
+
   const { scrollToElement } = useScroll()
 
-  return (    
+
+
+  return (
     <header className={styles.header}>
 
       <Link href="/" className={styles.logoWrapper}>
         <p className={styles.linkToHome}>elizabetebusevica.com</p>
       </Link>
 
-      <div className={styles.burger} onClick={() => {
-        // TODO: open modal for the buttons that are being hidden
-      }}>
+      <div className={styles.burger}
+        onClick={() => setMenuOpen(!isMenuOpen)}
+      >
         <svg width="25" height="16" viewBox="0 0 25 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <line y1="1" x2="25" y2="1" stroke="black" stroke-width="2"/>
-          <line y1="8" x2="25" y2="8" stroke="black" stroke-width="2"/>
-          <line y1="15" x2="25" y2="15" stroke="black" stroke-width="2"/>
+          <line y1="1" x2="25" y2="1" stroke="black" stroke-width="2" />
+          <line y1="8" x2="25" y2="8" stroke="black" stroke-width="2" />
+          <line y1="15" x2="25" y2="15" stroke="black" stroke-width="2" />
         </svg>
       </div>
       <div className={styles.headerButtons}>
-        {isHomePage ? 
-        <>
-          <a className={styles.headerButton} onClick={() => {scrollToElement('approach')}}>Approach</a>
-          <a className={styles.headerButton} onClick={() => {scrollToElement('projects')}}>Projects</a>
-          <a className={styles.headerButton} onClick={() => {scrollToElement('contacts')}}>Contacts</a>
-          <Link className={styles.headerButton} href="/cv">CV</Link>
-        </>
-        :
-        <>
-          <Link className={styles.headerButton} href="/#approach">Approach</Link>
-          <Link className={styles.headerButton} href="/#projects">Projects</Link>
-          <Link className={styles.headerButton} href="/#contacts">Contacts</Link>
-          <Link className={styles.headerButton} href="/cv">CV</Link>
-        </>
+        {isHomePage ?
+          <>
+            <a className={styles.headerButton} onClick={() => { scrollToElement('approach') }}>Approach</a>
+            <a className={styles.headerButton} onClick={() => { scrollToElement('projects') }}>Projects</a>
+            <a className={styles.headerButton} onClick={() => { scrollToElement('contacts') }}>Contacts</a>
+            <Link className={styles.headerButton} href="/cv">CV</Link>
+          </>
+          :
+          <>
+            <Link className={styles.headerButton} href="/#approach">Approach</Link>
+            <Link className={styles.headerButton} href="/#projects">Projects</Link>
+            <Link className={styles.headerButton} href="/#contacts">Contacts</Link>
+            <Link className={styles.headerButton} href="/cv">CV</Link>
+          </>
         }
-      </div>  
+      </div>
+
+      {isMenuOpen && <ModalMenu isOpen={isMenuOpen} closeModal={() => setMenuOpen(false)} />}
+
     </header>
   )
 }
---
+++ src/components/ModalMenu/ModalMenu.jsx
@@ -0,0 +1,54 @@
+'use client'
+import styles from './ModalMenu.module.css'
+import React from 'react'
+import { useScroll } from '@/context/Scroll'
+import { usePathname } from 'next/navigation'
+import Link from 'next/link'
+
+const ModalMenu = ({ isOpen, closeModal }) => {
+    const { scrollToElement } = useScroll()
+    const pathname = usePathname()
+    const isHomePage = pathname === '/';
+
+    if (!isOpen) {
+        return null
+    } else {
+        const handleClick = (section) => {
+            closeModal();
+            scrollToElement(section);
+        };
+
+        return (
+            <div className={styles.modalOverlay}>
+                <div className={styles.top}>
+                    <a href="/" className={styles.homeButton}>elizabetebusevica.com</a>
+                    <button onClick={closeModal} className={styles.button}>
+                        <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M17.2929 18.7072L0.292906 1.70718L1.70712 0.292969L18.7071 17.293L17.2929 18.7072Z" fill="black" />
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M1.70709 18.7072L18.7071 1.70718L17.2929 0.292969L0.292877 17.293L1.70709 18.7072Z" fill="black" />
+                        </svg>
+                    </button>
+                </div>
+                <div className={styles.modalContent}>
+                {isHomePage ?
+                <>
+                    <a onClick={() => { handleClick('approach') }}>Approach</a>
+                    <a onClick={() => { handleClick('projects') }}>Projects</a>
+                    <a onClick={() => { handleClick('contacts') }}>Contacts</a>
+                    <a href="cv">CV</a>
+                </>
+                    :
+                <>
+                    <Link href="/#approach">Approach</Link>
+                    <Link href="/#projects">Projects</Link>
+                    <Link href="/#contacts">Contacts</Link>
+                    <Link href="/cv">CV</Link>
+                </>
+                }
+                </div>
+            </div>
+        )
+    }
+}
+
+    export default ModalMenu;
