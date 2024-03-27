6ffcff5f3094f700fd17dcf15917461a83f99649
Marta Locmele <martamarijalocmele@gmail.com>
2024-03-27 22:34:32 +0200

    fixed text

--- src/components/Introduction/Introduction.jsx
+++ src/components/Introduction/Introduction.jsx
@@ -71,12 +71,11 @@ export default function Introduction() {
     <div className={styles.subText} ref={scrollRef} id={id}>
     <p className={styles.subTextLeft}>
       In times when we consume large amounts of not only raw materials,
-  but also information, I believe that the most compelling story can be 
-  told with minimal number of elements. Therefore, I put this as my priority – to think about sustainability in the context of digital design and to resist short-term trends.
+      but also information, I believe that the most compelling story can be 
+      told with minimal number of elements. Therefore, I put this as my priority – to think about sustainability in the context of digital design.
   </p>
   <p className={styles.subTextRight}>
-  Through empathy and my interest in society, it’s well-being, as well as 
-the rational approach that is present in my life, I have developed an interest in UX/UI design. I tend to make my solutions interactive to start conversations about socially important topics.
+      Through empathy and my interest in society and its well-being, I have developed an interest in human-centered design, especially UX/UI. I am interested in designing solutions that not only fulfill functional needs but also initiate meaningful conversations about socially important topics.
   </p>
   </div>
 </div>
